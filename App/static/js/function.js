2cdc2dbf70513cd893edcbd83be6e14245ae30c7
Shaker Ahmed <99150959+Kingshak03@users.noreply.github.com>
2023-10-22 23:18:02 -0400

    Update function.js
    Added Like Post method and unlike method. Added button for bothj
    
    Co-Authored-By: sharian123 <112531519+sharian123@users.noreply.github.com>

--- App/static/js/function.js
+++ App/static/js/function.js
@@ -1,3 +1,26 @@
+function likePost(post_id){
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(this.response);
+        }
+    }
+    request.open("POST", "/like-post/" + post_id);
+    request.send();
+}
+
+function unlikePost(post_id){
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(this.response);
+        }
+    }
+    request.open("POST", "/unlike-post/" + post_id);
+    request.send();
+}
+
+
 function postHTML(postJSON) {
     const username = postJSON.username;
     const title = postJSON.title;
@@ -7,6 +30,8 @@ function postHTML(postJSON) {
     let postHTML = "<div class='post'>";
     postHTML += "<p><b><u>User:</u></b> " + username + " <b><u> Title: </u></b> " + title + "</p>"
     postHTML += "<p><b>Description: </b>" + description + "</p>"
+    postHTML += "<br><button onclick='likePost(\"" + post_id + "\")'>Like</button> ";
+    postHTML += "<br><button onclick='unlikePost(\"" + post_id + "\")'>Unlike</button> ";
     postHTML += "</div>";
     return postHTML;
 }
