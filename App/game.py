640e5b27d40fe34a5cd32637ec671621fa45bc11
Sploder12 <trevorschupbach@gmail.com>
2023-11-08 12:50:45 -0500

    turn logic

--- App/game.py
+++ App/game.py
@@ -15,8 +15,10 @@ class Gamestate:
         self.p2 = p2
         
         self.turn = RED
+        self.turn_num = 0
 
         self.over = False
+        self.winner = "<"
 
         self.board = [EMPTY for _ in range(0, Gamestate.SPOTS)]
         self.heights = [0 for _ in range(0, Gamestate.HEIGHT)]
@@ -24,8 +26,24 @@ class Gamestate:
     def can_place(self, x: int):
         if (x < 0 or x >= Gamestate.WIDTH):
             return False
-        
+
         return self.heights[x] < Gamestate.HEIGHT
+
+    def get_turn_player(self):
+        if (self.turn == RED):
+            return self.p1
+
+        return self.p2
+
+    def is_over(self):
+        if (self.turn_num >= Gamestate.WIDTH * Gamestate.HEIGHT):
+            return True
+
+        if (self.winner != "<"):
+            return True
+
+        return self.over
+
 
     # useful for saving to database
     def as_obj(self):
@@ -34,6 +52,7 @@ class Gamestate:
             "player2": self.p2,
             "turn": self.turn,
             "over": self.over,
+            "winner": self.winner,
             "board": self.board
         }
 
@@ -47,6 +66,8 @@ class Gamestate:
         # this column is full! (or out of range)
         if (not self.can_place(x)):
             return False
+
+        self.turn_num += 1
 
         # place the marker
         height = self.heights[x]
@@ -75,7 +96,6 @@ class Gamestate:
         if (height >= 3):
             consecutive = 0
 
-            
             startY = max(height - 3, 0)
             endY = min(height + 3, Gamestate.HEIGHT - 1)
 
@@ -109,7 +129,7 @@ class Gamestate:
                     if (consecutive >= 4):
                         self.over = True
                         return True
-                    
+
                 else:
                     consecutive = 0
 
@@ -120,16 +140,46 @@ class Gamestate:
                     if (consecutived >= 4):
                         self.over = True
                         return True
-                    
+
                 else:
                     consecutived = 0
 
         return True
 
+    # returns a string describing the result of the attempted move
+    # if the string is a player name, they have won
+    # invalid turns start with '<', valid turns start with '>'
+    # this is to avoid having a string that is a username (< and > are escaped)
+    def try_turn(self, x: int):
+        if (self.is_over()):
+            return "<Game Already Over"
+
+        # attempt to place
+        if (not self.place(x)):
+            return "<Bad Place"
+
+        # did that place win the game?
+        if (self.is_over()):
+            if (self.over):
+                self.winner = self.get_turn_player()
+                return self.winner
+
+            return ">tie"
+
+        # switch turn
+        if (self.turn == RED):
+            self.turn = YELLOW
+        else:
+            self.turn = RED
+
+        return ">turn"
+
 # useful for loading from database
 def from_obj(obj):
     out = Gamestate(obj["player1"], obj["player2"])
     out.turn = obj["turn"]
+
+    out.winner = obj["winner"]
     out.over = obj["over"]
 
     out.board = obj["board"]
@@ -138,6 +188,7 @@ def from_obj(obj):
         base = y * Gamestate.WIDTH
         for x in range(0, Gamestate.WIDTH):
             if (out.board[base + x] != EMPTY):
+                out.turn_num += 1
                 out.heights[x] += 1
 
     return out
