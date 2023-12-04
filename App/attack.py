577e7157864530a78717e457742e0134d9f7c255
abirxlr8 <yumadragneelxtray@gmail.com>
2023-12-04 00:48:35 -0500

    using nginx for rate limiting
    need to figure out error handling

--- App/attack.py
+++ App/attack.py
@@ -6,6 +6,7 @@ from flask_limiter.util import get_remote_address
 
 def get_ip():
     return request.headers.get('X-Real-IP')
+    #gets the ip check nginx.conf 
 
 def limit_rate(ip_address):
     None
