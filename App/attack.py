5108dec80c25da60558b53ede326590f342810af
abirxlr8 <yumadragneelxtray@gmail.com>
2023-12-04 04:16:46 -0500

    Update attack.py
    still figuring out logic

--- App/attack.py
+++ App/attack.py
@@ -3,7 +3,7 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 
-
+ip_dictionary = {}
 total_request = 50  #requests
 allowed_time = 10 #seconds 
 ip_block = 30 #seconds 
@@ -12,8 +12,19 @@ def get_ip():
     return request.headers.get('X-Real-IP')
     #gets the ip check nginx.conf 
 
+def handling_function(client_record,ip_address):
+    if(client_record['is_blocked']):
+        None
+
 def limit_rate(ip_address):
-    None
+    if(ip_address not in ip_dictionary):
+        record = {'requests': 0, 'blocked_time': 0, 'request_time_period': None,
+                  'is_blocked':False}
+        ip_dictionary[ip_address] = record
+
+    client_record = ip_dictionary[ip_address]
+    handling_function(client_record,ip_address)
+
 
 def overload_response():
     response = make_response(redirect('/'), 'Too Many Requests. Please try again later.')
