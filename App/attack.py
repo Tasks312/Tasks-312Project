9922a7e2534d795bc49356f0d270562292564a80
abirxlr8 <yumadragneelxtray@gmail.com>
2023-12-04 03:15:39 -0500

    back to flask
    coudlnt figure out blocking with nignx so using flask again

--- App/attack.py
+++ App/attack.py
@@ -1,8 +1,12 @@
-from flask import Flask,request
+from flask import Flask,request,make_response,redirect
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 
+
+total_request = 50  #requests
+allowed_time = 10 #seconds 
+ip_block = 30 #seconds 
 
 def get_ip():
     return request.headers.get('X-Real-IP')
@@ -10,3 +14,8 @@ def get_ip():
 
 def limit_rate(ip_address):
     None
+
+def overload_response():
+    response = make_response(redirect('/'), 'Too Many Requests. Please try again later.')
+    response.status_code = 429
+    return response
